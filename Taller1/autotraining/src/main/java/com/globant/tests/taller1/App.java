f7dfed4acd222241f18e290d0dfeeff87159b78a
nmelo526 <62447752+nmelo526@users.noreply.github.com>
2020-03-30 10:11:57 -0500

    Carga archivos Taller1

---
+++ Taller1/autotraining/src/main/java/com/globant/tests/taller1/App.java
@@ -0,0 +1,13 @@
+package com.globant.tests.taller1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
